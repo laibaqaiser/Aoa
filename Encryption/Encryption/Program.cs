274bd31c653449db3a8ad353aef82af6b6310c91
laibaqaiser <49567128+laibaqaiser@users.noreply.github.com>
2019-11-21 11:25:20 +0500

    frequency count

---
+++ Encryption/Encryption/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString;
+            Console.Write("Enter a string - ");
+            testString = Console.ReadLine();
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+            for(int i =0;i< testString.Length;i++)
+            {
+                if(!frequency.ContainsKey(testString[i]))
+                {
+                    frequency.Add(testString[i],0);
+                }
+                frequency[testString[i]]++;
+            }
+
+              foreach (var value in frequency.Values)
+            {
+                Console.WriteLine("Value of the Dictionary Item is: {0}", value);
+            }
+            Console.WriteLine("You entered '{0}'", testString);
+        }
+    }
+}
