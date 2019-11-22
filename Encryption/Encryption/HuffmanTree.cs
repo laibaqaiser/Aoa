65b625ae14b07748fc8ebaf2fe3072d439959ee7
laibaqaiser <49567128+laibaqaiser@users.noreply.github.com>
2019-11-22 21:23:55 +0500

    error removed

--- Encryption/Encryption/HuffmanTree.cs
+++ Encryption/Encryption/HuffmanTree.cs
@@ -12,30 +12,27 @@ namespace Encryption
         private Nodes root;
         Nodes n = new Nodes();
         private List<Nodes> nodes = new List<Nodes>();
-<<<<<<< HEAD
+
         private List<bool> encoding = new List<bool>();
-       public  void Encoding(string testString)
+        public void Encoding(string testString)
         {
             /*List<bool> e=Root.PrintTree(testString[3], new List<bool>());
             encoding.AddRange(e);*/
-           
-            
-            foreach(var v in testString)
+
+
+            foreach (var v in testString)
             {
-                List<bool> e =this.Root.PrintTree(v,new List<bool> ());
+                List<bool> e = this.Root.PrintTree(v, new List<bool>());
                 encoding.AddRange(e);
             }
-            foreach (var v  in encoding)
+            foreach (var v in encoding)
             {
                 Console.Write((v ? 1 : 0) + "");
             }
-            
-        }       
-=======
-        private Nodes root;
 
->>>>>>> 44f65a0ddd3604b75482c1fcd66ec9567990a75e
-        internal Nodes Root
+        }
+
+      internal Nodes Root
         {
             get
             {
@@ -48,10 +45,7 @@ namespace Encryption
             }
         }
 
-<<<<<<< HEAD
-       
-=======
->>>>>>> 44f65a0ddd3604b75482c1fcd66ec9567990a75e
+
         public void tree(string testString)
         {
 
@@ -77,69 +71,49 @@ namespace Encryption
                 n.Frequency = letter.Value;
                 nodes.Add(n);
             }
-<<<<<<< HEAD
-             while (nodes.Count>1)
-             {
-                 List<Nodes> orderedNodes = nodes.OrderBy(nodes => nodes.Frequency).ToList<Nodes>();
-                 if (orderedNodes.Count >= 2)
-                 {
-                     var element1 = orderedNodes[0];
-                     var element2 = orderedNodes[1];
-=======
 
             
-                while(nodes.Count > 1)
-             {
-                List<Nodes> orderedNodes = nodes.OrderBy(nodes => nodes.Frequency).ToList<Nodes>();
-                if (orderedNodes.Count >= 2)
-                {
-                    var element1 = orderedNodes[0];
-                    var element2 = orderedNodes[1];
->>>>>>> 44f65a0ddd3604b75482c1fcd66ec9567990a75e
-                    Nodes parentNode = new Nodes();
-                    parentNode.Letter = 'p';
-                    parentNode.Frequency = element1.Frequency + element2.Frequency;
-                    parentNode.Left = element1;
-                    parentNode.Right = element2;
-<<<<<<< HEAD
-                    
-=======
+                while (nodes.Count > 1)
+                    {
+                        List<Nodes> orderedNodes = nodes.OrderBy(nodes => nodes.Frequency).ToList<Nodes>();
+                        if (orderedNodes.Count >= 2)
+                        {
+                            var element1 = orderedNodes[0];
+                            var element2 = orderedNodes[1];
 
->>>>>>> 44f65a0ddd3604b75482c1fcd66ec9567990a75e
-                    nodes.Add(parentNode);
-                    nodes.Remove(element1);
-                    nodes.Remove(element2);
+                            Nodes parentNode = new Nodes();
+                            parentNode.Letter = 'p';
+                            parentNode.Frequency = element1.Frequency + element2.Frequency;
+                            parentNode.Left = element1;
+                            parentNode.Right = element2;
+
+                            nodes.Add(parentNode);
+                            nodes.Remove(element1);
+                            nodes.Remove(element2);
+                        }
+
+                        this.Root = nodes.FirstOrDefault();
+                    }
+
+
+
+                    int p1 = 0;
+                    foreach (Nodes n in nodes)
+                    {
+                        Console.Write("At Position {0}: ", p1);
+                        Console.WriteLine(n.Letter);
+                        Console.WriteLine(n.Frequency);
+                        p1++;
+                    }
+                    this.Root = nodes.FirstOrDefault();
                 }
-<<<<<<< HEAD
-                 this.Root = nodes.FirstOrDefault();
-             }
-            
 
 
-            int p1 = 0;
-                foreach (Nodes n in nodes)
-                {
-                    Console.Write("At Position {0}: ", p1);
-                    Console.WriteLine(n.Letter);
-                    Console.WriteLine(n.Frequency);
-                    p1++;
-                }
-            
-=======
-                this.Root = nodes.FirstOrDefault();
+                
+
             }
-            
 
-            int p1 = 0;
-            foreach (Nodes n in nodes)
-            {
-                Console.Write("At Position {0}: ", p1);
-                Console.WriteLine(n.Letter);
-                Console.WriteLine(n.Frequency);
-                p1++;
-            }
->>>>>>> 44f65a0ddd3604b75482c1fcd66ec9567990a75e
         }
-       
-    }
-}
+    
+
+
--- Encryption/Encryption/Program.cs
+++ Encryption/Encryption/Program.cs
@@ -11,13 +11,11 @@ namespace Encryption
         
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-           HuffmanTree huffmantree = new HuffmanTree();
-            string testString = System.IO.File.ReadAllText(@"C:\Users\MAHNOOR\Desktop\Encryption\Aoa\Encryption\Encryption\code.txt");
-=======
+
+           
             HuffmanTree huffmantree = new HuffmanTree();
             string testString = System.IO.File.ReadAllText(@"C:\Users\Muhammad Muzzamil\Documents\AoaProject\Aoa\Encryption\Encryption\code.txt");
->>>>>>> 44f65a0ddd3604b75482c1fcd66ec9567990a75e
+
             huffmantree.tree(testString);
             huffmantree.Encoding(testString);
 
