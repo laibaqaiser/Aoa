aac0e33e55b10b97c9ae148600a0539bad402939
Mahnoor17 <56605879+Mahnoor17@users.noreply.github.com>
2019-11-23 18:57:30 +0500

    Key for decoding

--- Encryption/Encryption/Program.cs
+++ Encryption/Encryption/Program.cs
@@ -14,7 +14,17 @@ namespace Encryption
             string testString = System.IO.File.ReadAllText(@"C:\Users\MAHNOOR\Desktop\Encryption\Aoa\Encryption\Encryption\code.txt");
             huffmantree.tree(testString);
             huffmantree.Encoding(testString);
-            huffmantree.Decoding();
+            Console.WriteLine("");
+            Console.Write("Enter key to decode ");
+            string key = Console.ReadLine();
+            if (key == "123")
+            {
+                huffmantree.Decoding();
+            }
+            else
+            {
+                Console.WriteLine("Invalid key!!!");
+            }
         }
     }
 }
